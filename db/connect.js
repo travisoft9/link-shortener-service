15d0da871b3cede8cc0a7acb26dbfdb6dc42c82e
Travis Richards <travisoft9@gmail.com>
2019-08-22 06:04:34 -0700

    Add db module with connect method

---
+++ db/connect.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const config = require('config')
+const mongoUri = config.get('mongoURI')
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(mongoUri, {
+      useNewUrlParser: true
+    })
+  } catch (error) {
+    console.error(`There was an error connecting to MongoDB at ${mongoUri}.`)
+    console.error(error)
+    process.exit(1)
+  }
+}
+
+module.exports = connectDB
