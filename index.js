74ddbe825c5f2b56b4bc81d55ee134f0084f4dd5
Travis Richards <travisoft9@gmail.com>
2019-08-22 06:04:49 -0700

    App connects to db

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('goodbye')
+const express = require('express')
+const config = require('config')
+const db = require('./db')
+
+const app = express()
+
+app.use(express.json({ extended: false }))
+
+const port = config.get('PORT')
+
+db.connect()
+
+app.listen(port, () => console.log(`server listening on port ${port}`))
