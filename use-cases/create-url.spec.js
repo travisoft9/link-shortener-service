93f83c0693e387b2c81e6718045536d210cf7db8
Travis Richards <travisoft9@gmail.com>
2019-08-23 04:24:23 -0700

    Add test:

--- use-cases/create-url.spec.js
+++ use-cases/create-url.spec.js
@@ -39,34 +39,50 @@ afterAll(async () => {
 
 const FAKE_SHORT_CODE = 'aaa'
 const FAKE_BASE_URL = 'http://localhost:5000'
+const longUrl = 'https://www.google.com'
+const date = Date.now()
 let createUrl
 
 beforeEach(() => {
   createUrl = buildCreateUrl({
     saveUrl: db.Url.create,
     isValidUrl: validUrl.isUri,
-    
+
     // mock shortid because it creates random ids
     createUrlCode: () => FAKE_SHORT_CODE,
     baseUrl: FAKE_BASE_URL
   })
 })
 
-it('throws if baseUrl is not a valid uri', () => {
+afterEach(async () => {
+  await mongoose.connection.dropDatabase()
+})
+
+test('throws if baseUrl is not a valid uri', () => {
   return expect(createUrl('notvaliduri')).rejects.toThrow(
     '"notvaliduri" is not a valid URL.'
   )
 })
 
-it('resolves promise for new url', async () => {
-  const date = Date.now()
-  const url = await createUrl('https://www.google.com', date)
+test('resolves promise for new url', async () => {
+  const url = await createUrl(longUrl, date)
   expect(url).toEqual({
-    longUrl: 'https://www.google.com',
+    longUrl,
     date: date.toString(),
     urlCode: FAKE_SHORT_CODE,
     shortUrl: `${FAKE_BASE_URL}/${FAKE_SHORT_CODE}`
   })
 })
-it.todo('adds url to database')
-it.todo('must not be an existing url with same baseUrl')
+
+test('adds url to database', async () => {
+  await createUrl(longUrl, date)
+  const url = await db.Url.find({longUrl})
+  expect(url[0]).toMatchObject({
+    longUrl,
+    date: date.toString(),
+    urlCode: FAKE_SHORT_CODE,
+    shortUrl: `${FAKE_BASE_URL}/${FAKE_SHORT_CODE}`
+  })
+})
+
+test.todo('must not be an existing url with same baseUrl')
